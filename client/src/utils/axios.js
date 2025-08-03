d7a8e7cb02cfe8e7aa084959ea950f944561b942
shreyash vishwakarma <shreyasvishwakarma00@gmail.com>
2025-08-03 16:48:41 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://fcrm-02g1.onrender.com/api", // ✅ your Render backend
+  baseURL: "https://fcrm-02g1.onrender.com", // ✅ your Render backend
   withCredentials: true, // ✅ needed for cookies/sessions
 });
 
